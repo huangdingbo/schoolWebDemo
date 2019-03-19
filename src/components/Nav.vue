5f1771894c4e8d9dbf44ac489b96b5dc7e3c4c1b
liupeng <651386852@qq.com>
2019-03-19 18:27:13 +0800

    company

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -6,16 +6,40 @@
                 <div class="nav-title">智慧校园</div>
                 <div class="nav-list">
                     <div class="nav-item" @click="jump(1)" @mouseenter="pointEnter(1)" @mouseleave="pointOut" :class="{ac:this.ac==1}">
-                        <img src="" width="400" height="300">
-                        <p>模块1</p>
+                        <div class="svg_icon" id="svg_x">
+                            <svg  version="1.2" baseProfile="tiny" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 48 48" xml:space="preserve"><g>
+                                    <circle fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" cx="24" cy="24" r="23"/>
+                                    <path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" stroke-dasharray="4" d="M11.3,24c0-7,5.7-12.7,12.7-12.7S36.7,17,36.7,24"/>
+                                    <path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" d="M42.7,24c0-10.3-8.4-18.7-18.7-18.7S5.3,13.7,5.3,24c0,2.4,0.5,4.7,1.3,6.8h34.9C42.3,28.7,42.7,26.4,42.7,24z"/>
+                                    <path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" d="M20.4,30.4c0-2,1.6-3.6,3.6-3.6s3.6,1.6,3.6,3.6"/>
+                                    <line fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" x1="22.8" y1="26.9" x2="17.2" y2="10.9"/></g>
+                            </svg>
+                        </div>
+                        <p>智慧校园驾驶舱</p>
                     </div>
                     <div class="nav-item" @click="jump(2)" @mouseenter="pointEnter(2)" @mouseleave="pointOut" :class="{ac:this.ac==2}">
-                        <img src="" width="400" height="300">
-                        <p>模块2</p>
-                    </div>
+                        <div class="svg_icon">
+                            <svg version="1.2" baseProfile="tiny" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"x="0px" y="0px" viewBox="0 0 48 48" xml:space="preserve">
+                                <circle fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" cx="24" cy="24" r="23"/>
+                                <polyline fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="45.6,24 24,24 24,45.8 "/>
+                                <line fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="24" y1="24" x2="36.8" y2="6.2"/>
+                                <line fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" x1="9" y1="6.6" x2="24" y2="24"/>
+                                <path fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" d="M39.2,24c0,8.4-6.8,15.2-15.2,15.2S8.8,32.4,8.8,24c0-4.6,2-8.7,5.3-11.5"/>
+                            </svg>
+                        </div>
+                        <p>分析研判系统</p>
+                        </div>
                     <div class="nav-item" @click="jump(3)" @mouseenter="pointEnter(3)" @mouseleave="pointOut" :class="{ac:this.ac==3}">
-                        <img src="" width="400" height="300">
-                        <p>模块3</p>
+                        <div class="svg_icon">
+                            <svg version="1.2" baseProfile="tiny" id="Layer_3" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"x="0px" y="0px" viewBox="0 0 48 48" xml:space="preserve"><g><path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" d="M38,12.7c0,1-0.4,1.9-1,2.5s-1.5,1-2.5,1c-2,0-3.6-1.6-3.6-3.6c0-0.8,0.3-1.6,0.8-2.2c0.7-0.8,1.7-1.3,2.8-1.3C36.4,9.2,38,10.8,38,12.7z"/>
+                                <circle fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" cx="13.5" cy="35.3" r="3.6"/>
+                                <circle fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" cx="24" cy="24" r="23"/><path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" d="M36.9,15.3c1.7,2.5,2.6,5.5,2.6,8.7c0,8.6-7,15.5-15.5,15.5c-2.9,0-5.6-0.8-8-2.2"/><path fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" d="M11.3,33c-1.8-2.5-2.9-5.6-2.9-9c0-8.6,7-15.5,15.5-15.5c3,0,5.8,0.8,8.1,2.3"/>
+                                <circle fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" cx="24" cy="24" r="6.9"/>
+                                <line fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" x1="1" y1="24" x2="47" y2="24"/>
+                                <line fill="none" stroke="#fff" stroke-width="2" stroke-linejoin="round" stroke-miterlimit="10" x1="24" y1="1" x2="24" y2="47"/></g>
+                            </svg>
+                        </div>
+                        <p>监控预警系统</p>
                     </div>
                 </div>
             </div>
@@ -24,6 +48,8 @@
 </template>
 
 <script>
+    import Vivus from "../plugins/vivus"
+
     export default {
         name: "Nav",
         props:{
@@ -56,9 +82,17 @@
             },
             pointEnter:function (index) {
                 this.ac = index;
+                this.play(index)
             },
             pointOut:function () {
                 this.ac = '';
+            },
+            play(index){
+                const test = new Vivus('Layer_'+index,{
+                    type: 'delayed',
+                    duration: 70,
+                    animTimingFunction: Vivus.EASE
+                });
             }
         }
     }
@@ -72,8 +106,9 @@
     .nav-title{text-align: center;font-size: 48px;font-weight: bold;color: #d6e9ff;margin-top:60px;
         text-shadow: 0 0 20px #145f87, 0 0 20px #145f87, 0 0 20px #145f87;}
     .nav-list{margin-top:60px;display: flex;justify-content: space-around;}
-    .nav-item {font-size: 30px;font-weight: bold;color: #fff;
-        text-shadow: 0 0 15px #0b599c, 0 0 30px #0b599c, 0 0 30px #0b599c, 0 0 60px #0b599c;}
+    .nav-item {font-size: 30px;font-weight: bold;color: #fff;background: url("../assets/nav_card.png");background-size: 100% 100%;
+        text-shadow: 0 0 15px #0b599c, 0 0 30px #0b599c, 0 0 30px #0b599c, 0 0 60px #0b599c;padding:80px 50px}
     .nav-item p{margin-top: 30px;}
     .nav-item.ac img{transform: scale(1.1);border:1px solid red}
+    .svg_icon{width: 260px}
 </style>
--- src/views/Warning.vue
+++ src/views/Warning.vue
@@ -9,23 +9,42 @@
             </div>
         </div>
         <div class="warning_cont">
+            <div class="warning_title">
+                <div class="warning_grade">
+                    <el-dropdown trigger="click"  @command="gradeSel" placement="bottom-start">
+                            <span class="el-dropdown-link">
+                                <!--{{subjectName}}-->
+                                年级
+                            </span>
+                        <el-dropdown-menu slot="dropdown" >
+                            <el-dropdown-item v-for="item in gradeList" :command="item.value" >{{item.name}}</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </el-dropdown>
+                </div>
+                <div class="warning_topic">监控预警总体情况</div>
+            </div>
             <router-view></router-view>
         </div>
         <div class="warning_work">
             <div class="work_title">预警工作台</div>
             <div class="work_list">
                 <div class="work_item" v-for="item in list">
-                    <img src="../assets/warning/warn_badge_red.png">
-                    <div class="name">{{item.name}}</div>
+                    <div class="name"><img src="../assets/warning/warn_badge_red.png">{{item.name}}<div class="btn" :data-id="item.id">查看详情</div></div>
                     <div class="info">{{item.value}}</div>
-                    <div class="btn">查看详情</div>
                 </div>
             </div>
+            <div class="work_btn">查看全部</div>
         </div>
     </div>
 </template>
 
 <script>
+    import Vue from "vue";
+    import { Dropdown, DropdownMenu, DropdownItem } from "element-ui";
+    Vue.use(Dropdown);
+    Vue.use(DropdownMenu);
+    Vue.use(DropdownItem);
+
     export default {
         name: "Warning",
         components: {
@@ -33,6 +52,7 @@
         data() {
             return {
                 list:[],
+                gradeList:[],
             };
         },
         mounted() {
@@ -40,25 +60,42 @@
         },
         methods: {
             init(){
-                this.$api.work({type:this.subject}).then(res => {
-                    this.list = res.list
-                    console.log(res);
+                this.$api.work().then(res => {
+                    this.list = res.list;
                 });
-            }
+                this.$api.gradelist().then(res => {
+                    console.log(res);
+                    this.gradeList = res.list;
+                });
+                this.$api.gradelist().then(res => {
+                    console.log(res);
+                    this.gradeList = res.list;
+                });
+            },
+            gradeSel(){
+
+            },
         }
     }
 </script>
 
 <style scoped>
+    @import "../css/dropdown.css";
     .warning{display: flex;width: 100%;height:100%;}
     .title{margin-top:40px}
     .warning_nav{width: 400px;height:100%;border-right:2px solid #0a5b85}
     .warning_list{display: flex;flex-direction: column;font-size: 24px;font-weight: bold;margin-top:30px}
     .warning_item{display: flex;align-items: center;height:90px;justify-content: center;font-size: 32px}
     .warning_cont{width: 1200px;}
-
-
-    .warning_work{width: 300px;background: rgba( 1, 16, 29, 0.851 );border-left:2px solid #0a5b85;padding:40px 0 0 50px;}
+    .warning_title{display: flex;align-items: center;justify-content: center;margin-top:40px;}
+    .warning_grade{margin-right:30px;}
+    .warning_topic{font-size: 46px;color:#fff;display:inline-block;border-bottom: 3px solid rgb( 110, 194, 255 );}
+    .warning_work{width: 360px;background: rgba( 1, 16, 29, 0.851 );border-left:2px solid #0a5b85;padding:40px 0 0 20px;}
     .work_title{font-size: 26px;text-shadow: 0 0 20px #145f87, 0 0 20px #145f87, 0 0 20px #145f87;font-weight: bold;color: #d6e9ff;text-align: left}
-    .work_list{height:600px;overflow: auto}
+    .work_list{height:600px;overflow: auto;margin-top:30px;}
+    .work_item{padding:10px 0;font-size: 20px;color:#fff;border-top:2px solid #2a3c48}
+    .name{display: flex;align-items: center;justify-content: space-around;font-size: 18px;padding:10px 0}
+    .btn{border: 1px solid #2a3c48;font-size: 16px;padding:2px 5px;border-radius: 5px;background-color: #162736;cursor: pointer}
+    .work_btn{color: #fff;font-weight: bold;background-color: #1373d1;border-radius: 4px;font-size: 20px;width: 180px;display: inline-block;
+        padding:5px 0;margin-top:20px;cursor: pointer}
 </style>
