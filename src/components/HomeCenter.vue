8ff2fd9ff10a4a28515d3831f588511d1fa53bb9
liupeng <651386852@qq.com>
2019-03-26 18:59:29 +0800

    company

--- src/components/HomeCenter.vue
+++ src/components/HomeCenter.vue
@@ -1,9 +1,12 @@
 <template>
     <div class="index-center" >
         <div class="center-box" v-for="(item,index) in test.slice(0, 10)">
-            <img :src="item.pic">
-            <div class="num">{{item.total}}分</div>
-            <div class="name">{{item.name}} {{item.banji}}</div>
+            <img class="box_bg" src="../assets/home/box.png">
+            <div class="box">
+                <img :src="item.pic">
+                <div class="num">{{item.total}}分</div>
+                <div class="name">{{item.name}} {{item.banji}}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,8 +30,11 @@
 <style scoped>
     .index-center{position: absolute;top:100px;left:450px;width: 1000px;height:750px;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-around;
         border: 1px solid #19516f;border-radius: 5px;background-color: rgba(3, 16, 23, 0.702);box-shadow: 0px 0px 10px 0 #7bd4fc inset;}
-    .center-box{background: url("../assets/home/box.png");width:200px;height: 290px;background-size: 100%;display: flex;flex-direction: column;align-items: center; }
-    .center-box img{width:130px;height: 176px;margin-top:24px}
-    .num{background: rgba(0,0,0,1);color: #0E18EF;width: 130px;font-size: 26px;}
-    .name{height: 30px;color: #fff;margin-top: 18px;}
+    .center-box{display: flex;flex-direction: column;align-items: center;position: relative }
+    /*.center-box img{width:130px;height: 176px;margin-top:24px}*/
+    .box_bg{width: 200px;height: 280px;z-index: 2;}
+    .box{position: absolute;width: 146px;margin-top:20px;overflow: hidden}
+    .box img{height: 150px;position: relative;left: -25%;}
+    .num{background:rgb(1,4,6);color: #27A9FF;font-size: 26px;position: relative;top:-4px;height: 37px;}
+    .name{height: 30px;color: #fff;padding-top: 18px;background: linear-gradient(bottom,#392005,#211408)}
 </style>
